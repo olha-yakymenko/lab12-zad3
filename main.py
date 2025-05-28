de3e2ed32b8ab44de1f7b7ffd828e6f94c3a01ed
Olha Yakymenko <olhayakymenko@MacBook-Air-Olha.local>
2025-05-28 22:48:37 +0200

    zad3

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import platform
 
 system = platform.system()
-machine = platform.machine()
-
 
 if system == "Windows":
     print("Witaj w systemie Windows!")
 elif system == "Linux":
-    arch = "ARM" if "ARM" in machine.upper() else "AMD64"
-    print(f"Witaj w systemie Linux na architekturze {arch}!")
+    print("Witaj w systemie Linux!")
 else:
     print(f"Witaj w systemie {system}!")
